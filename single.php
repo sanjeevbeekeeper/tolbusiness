7aa7d234212a6b0377c215346a527bfc757a1cf5
Sanjeev Beekeeper PC office <sanjeevbeekeeper@gmail.com>
2017-11-08 11:50:36 +0530

    Moved the content to new page and link with get_template_part

--- single.php
+++ single.php
@@ -4,81 +4,81 @@
     <div class="single_container">
         <div class="row">
 
-            <div class="col-lg-8 col-lg-offset-2">
-                <div class="row">
-                    <!-- Featured image -->
-                    <div class="col-lg-6 text-right">
-                        <?php the_post_thumbnail('large'); ?>
-                    </div> <!-- //.col -->
-
-                    <!-- Meta container -->
-                    <div class="col-lg-6">
-                        <div class="position_container">
-                            <div class="meta_container">
-                                <div class="back-btn">
-                                    <a href="<?php echo get_page_link( get_page_by_title( product )->ID ); ?>">Back to Gallery</a>
-                                </div>
-
-                                <?php
-                                    if ( have_posts() ) : while ( have_posts() ) : the_post();
-                                    ?>
-
-                                    <!-- code -->
-                                    <?php the_title('<h4 class="page-header">', '</h4>'); ?>
-
-                                    <!-- content -->
-                                    <div class="content" style="color: red;">
-                                        <!-- <?php the_content(); ?> -->
+            <div class="product_container ">
+                <div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1 col-xs-12">
+                    <div class="row">
+                        <!-- Featured image -->
+                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                            <div class="image_container"><?php the_post_thumbnail('large'); ?></div>
+                        </div> <!-- //.col -->
+                        <!-- Meta container -->
+                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                            <div class="position_container">
+                                <div class="meta_container">
+                                    <div class="back-btn">
+                                        <a href="<?php echo get_page_link( get_page_by_title( product )->ID ); ?>">Back to Gallery</a>
                                     </div>
 
-                                    <!-- product name -->
-                                    <p>
-                                        <span>Product type: </span>
-                                        <?php
-                                            foreach((get_the_category()) as $category) {
-                                                echo $category->cat_name . ' ';
-                                                }
-                                            ?>
-                                    </p>
+                                    <?php
+                                        if ( have_posts() ) : while ( have_posts() ) : the_post();
+                                        ?>
 
-                                <!-- material -->
-                                    <p>
-                                        <!-- <span>Material: </span>
-                                        <?php
-                                            // $my_tags = get_the_tags();
-                                            // if ( $my_tags ) {
-                                                // foreach ( $my_tags as $tag ) {
-                                                    // $tag_names[] = $tag->name;
-                                                    // }
-                                                // echo implode( ' / ', $tag_names );
-                                                // }
-                                            ?> -->
-                                    </p>
+                                        <!-- code -->
+                                        <?php the_title('<h4 class="page-header">', '</h4>'); ?>
+
+                                        <!-- content -->
+                                        <div class="content" style="color: red;">
+                                            <!-- <?php the_content(); ?> -->
+                                        </div>
+
+                                        <!-- product name -->
+                                        <p>
+                                            <span>Product type: </span>
+                                            <?php
+                                                foreach((get_the_category()) as $category) {
+                                                    echo $category->cat_name . ' ';
+                                                    }
+                                                ?>
+                                        </p>
+
+                                    <!-- material -->
+                                        <p>
+                                            <span>Material: </span>
+                                            <?php
+                                                $my_tags = get_the_tags();
+                                                if ( $my_tags ) {
+                                                    foreach ( $my_tags as $tag ) {
+                                                        $tag_names[] = $tag->name;
+                                                        }
+                                                    echo implode( ' / ', $tag_names );
+                                                    }
+                                                ?>
+                                        </p>
 
 
-                                <!-- color -->
-                                <?php $color = get_post_meta($post->ID, '_productcolor_key', true); ?>
-                                <p><span>Color:</span> <?php echo $color ?></p>
+                                    <!-- color -->
+                                    <?php $color = get_post_meta($post->ID, '_productcolor_key', true); ?>
+                                    <p><span>Color:</span> <?php echo $color ?></p>
 
-                                <?php $size = get_post_meta($post->ID, '_productsize_key', true); ?>
-                                <!-- size -->
-                                <!-- <p><span>Size (inches): </span> <?php //echo $size ?></p> -->
-                                <!-- enquiry -->
-                                <!-- END LOOP -->
-                                <?php
-                                    endwhile;
-                                    else:
-                                        _e('Sorry, no produtcs matched your criteria.');
-                                    endif;
-                                    ?>
-                                <a href="<?php echo get_page_link( get_page_by_title( 'enquiry' )->ID ); ?>" class="btn btn-default">Enquiry</a>
+                                    <?php $size = get_post_meta($post->ID, '_productsize_key', true); ?>
+                                    <!-- size -->
+                                    <!-- <p><span>Size (inches): </span> <?php //echo $size ?></p> -->
+                                    <!-- enquiry -->
+                                    <!-- END LOOP -->
+                                    <?php
+                                        endwhile;
+                                        else:
+                                            _e('Sorry, no produtcs matched your criteria.');
+                                        endif;
+                                        ?>
+                                    <a href="<?php echo get_page_link( get_page_by_title( 'enquiry' )->ID ); ?>" class="btn btn-default">Enquiry</a>
+                                </div>
                             </div>
-                        </div>
-                    </div> <!-- //.col -->
+                        </div> <!-- //.col -->
 
-
-                </div> <!-- //.row -->
-            </div>
+                    </div> <!-- //.row -->
+                </div> <!-- //.col -->
+            </div> <!-- //.product_container -->
         </div>
     </div>
 
