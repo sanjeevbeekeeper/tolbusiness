977809de4a85c8b215d12d58464a88d745ac21e4
Sanjeev Beekeeper PC office <sanjeevbeekeeper@gmail.com>
2017-11-08 11:51:46 +0530

    Added beekeeper studio logo and fix the alignment

--- footer.php
+++ footer.php
@@ -1,12 +1,26 @@
 
                 <!-- footer -->
                 <footer>
-                    <?php wp_nav_menu( $args ); ?>
-                    <span class="small text-muted">
-                        Copyright <?php echo date('Y'); ?> &copy;
-                        tolbusiness.com. All rights reserved
-                    </span>
-                </footer>
+                    <div class="row">
+                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                            <nav>
+                                <?php wp_nav_menu( $args ); ?>
+                                <span class="small text-muted">
+                                    Copyright <?php echo date('Y'); ?> &copy;
+                                    tolbusiness.com. All rights reserved
+                                </span>
+                            </nav>
+                        </div> <!--//.col-lg-6 -->
+                        <div class="col-lg-6  col-md-6 col-sm-6 col-xs-12">
+                            <div class="copyright">
+                                <a href="http://www.beekeeperstudio.com/" target="_blank">
+                                    <span class="small text-muted">Designed by</span>
+                                    <img src="<?php bloginfo('template_directory'); ?>/resources/beekeeperstudio.png" alt="Beekeeper Studio Logo">
+                                </a>
+                            </div>
+                        </div> <!--//.col-lg-6 -->
+                    </div> <!--//.row -->
+                </footer> <!--//footer -->
 
             </div> <!-- //.col closing -->
         </div> <!-- //.row closing -->
